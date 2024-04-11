52a5c2f5cac895235c65c0226b390e6d4b4b88aa
rayen haddad <95473558+rayenhd@users.noreply.github.com>
2024-04-11 13:56:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,17 @@ import os
 import requests
 
 def get_weather(latitude, longitude):
-    api_key = "aaca89a14b3698bbd46dd966b2fa436d"
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}'
+    api_key = os.environ.get('OPENWEATHER_API_KEY')
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}'
     response = requests.get(url)
     data = response.json()
-
-    if response.status_code == 200:
-        return data
-    else:
-        raise Exception(f"Failed to retrieve weather data: {data['message']}")
+    return data
 
 if __name__ == "__main__":
-    latitude = float(input("Enter latitude: "))
-    longitude = float(input("Enter longitude: "))
+    latitude = float(os.environ.get("LAT", 0.0))
+    longitude = float(os.environ.get("LONG", 0.0))
+    weather_data = get_weather(latitude, longitude)
+    print(weather_data)
 
-    try:
-        weather_data = get_weather(latitude, longitude)
-        print("Weather data:")
-        print(weather_data)
-    except Exception as e:
-        print(f"Error: {e}")
+#https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=236df28640d8e0bc71a29bd329f6a8d7
+
